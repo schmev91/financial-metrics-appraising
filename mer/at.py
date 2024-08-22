c16101fb01131660368b6212eefc6addacbf60e7
schmev <86550672+schmev91@users.noreply.github.com>
2024-08-22 20:51:22 +0700

    asset turn over

--- mer/at.py
+++ mer/at.py
@@ -7,13 +7,31 @@ def basic(stm: Statement):
         net_sale = int(stm.income["Net Sales"])
         equity = int(stm.balance["TOTAL ASSETS (Bn. VND)"])
 
-        result = (net_sale / equity) * 100
+        if 0 in [net_sale, equity]:
+            raise Exception()
+
+        result = net_sale / equity
         return result
     except:
         return None
 
 
-formulars = [basic]
+def second(stm: Statement):
+    try:
+        equity = int(stm.balance["TOTAL ASSETS (Bn. VND)"])
+        sales = abs(int(stm.income["Sales"]))
+        deduction = abs(int(stm.income["Sales deductions"]))
+
+        if 0 in [sales, equity]:
+            raise Exception()
+
+        result = (sales - deduction) / equity
+        return result
+    except:
+        return None
+
+
+formulars = [basic, second]
 
 for stm in symbol_list:
     for formulation in formulars:
